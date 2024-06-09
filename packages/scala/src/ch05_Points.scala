4334d871c00dd46a6d494da30a38499bd443c93c
r-tamura <rtamura30@gmail.com>
2024-06-09 06:18:47 +0000

    5.23 コーヒーブレイク: フィルタイングの方法
    for内包表記でのフィルタリングの方法は3つある
    - List.filter
    - ガード式
    - flatMap関数に渡された関数
    最後の方法はコレクション型以外でも利用できる（今後の章で登場する）
    ❓ガード式の方法は直観的に感じたが、あまり一般的ではないらしい. なんでだろうか？

--- packages/scala/src/ch05_Points.scala
+++ packages/scala/src/ch05_Points.scala
@@ -2,6 +2,18 @@ case class Point(x: Int, y: Int)
 
 case class Point3d(x: Int, y: Int, z: Int)
 
+def isInside(point: Point, radius: Int): Boolean = {
+  radius * radius >= point.x * point.x + point.y * point.y
+}
+
+def insideFilter(point: Point, radius: Int): List[Point] = {
+  if (isInside(point, radius)) List(point) else List.empty
+}
+
+def validateRaidus(radius: Int): List[Int] = {
+  if (radius >= 0) List(radius) else List.empty
+}
+
 object ch05_Points extends App {
   // 5.12 実習: 入れ子のflatMap
   assert(
@@ -25,4 +37,39 @@ object ch05_Points extends App {
       Point3d(1, 7, 4)
     )
   )
+
+  // 5.23 コーヒーブレイク: フィルタリングの方法
+  {
+    val points = List(Point(5, 2), Point(1, 1))
+    val reskyReadiuses = List(-10, 0, 2)
+
+    val expected = List(
+      "Point(1,1) is within a radius of 2"
+    )
+
+    // Listのfilter関数
+    assert(
+      (for {
+        r <- reskyReadiuses.filter(r => r >= 0)
+        point <- points.filter(p => isInside(p, r))
+      } yield s"$point is within a radius of $r")
+        == expected
+    )
+    // ガード式
+    assert((for {
+      r <- reskyReadiuses if r >= 0
+      point <- points if isInside(point, r)
+    } yield s"$point is within a radius of $r") == expected)
+
+    // flatMap関数に渡された関数
+    assert(
+      (for {
+        r <- reskyReadiuses
+        validRadius <- validateRaidus(r)
+        point <- points
+        inPoint <- insideFilter(point, r)
+      } yield s"$inPoint is within a radius of $validRadius")
+        == expected
+    )
+  }
 }
