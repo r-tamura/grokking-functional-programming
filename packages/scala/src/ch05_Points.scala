f071b2e5b90f1f39056772f99e7105a58312786c
r-tamura <rtamura30@gmail.com>
2024-06-09 05:45:04 +0000

    5.15 コーヒーブレイク: flatMaptとfor内包表記
    - 内包表記を使うとインデントが深くなる問題を解消できる
    - ReScriptには内包表記がない

--- packages/scala/src/ch05_Points.scala
+++ packages/scala/src/ch05_Points.scala
@@ -1,5 +1,7 @@
 case class Point(x: Int, y: Int)
 
+case class Point3d(x: Int, y: Int, z: Int)
+
 object ch05_Points extends App {
   // 5.12 実習: 入れ子のflatMap
   assert(
@@ -8,5 +10,19 @@ object ch05_Points extends App {
         Point(x, y)
       })
     ) == List(Point(1, -2), Point(1, 7))
+  )
+
+  // 5.15 コーヒーブレイク: flatMapとfor内包表記
+  assert(
+    (for {
+      x <- List(1)
+      y <- List(-2, 7)
+      z <- List(3, 4)
+    } yield Point3d(x, y, z)) == List(
+      Point3d(1, -2, 3),
+      Point3d(1, -2, 4),
+      Point3d(1, 7, 3),
+      Point3d(1, 7, 4)
+    )
   )
 }
