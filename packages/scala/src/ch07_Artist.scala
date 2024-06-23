a1f332cea5389c78bd629662bad714e8aa568286
rutamu30 <rtamura30@gmail.com>
2024-06-23 04:30:39 +0000

    7.37 コーヒーブレイク: 設計と保守性
    - 自分の解答と書籍の模範解答をくらべて、変数名などがよりドメインレベルの言葉が使われていてわかりやすいと思った

--- packages/scala/src/ch07_Artist.scala
+++ packages/scala/src/ch07_Artist.scala
@@ -23,39 +23,138 @@ case class Artist(
     name: String,
     genre: MusicGenre,
     location: Location,
-    yearsActive: YearsActive
+    yearsActiveList: List[YearsActive]
 )
 
-def activeSearch(artist: Artist, currentYear: Int): Int = {
-  artist.yearsActive match {
-    case StillActive(since)        => currentYear - since
-    case ActiveBetween(start, end) => end - start
-  }
+def activeYears(artist: Artist, currentYear: Int): Int = {
+  artist.yearsActiveList.foldLeft(0)((total, range) =>
+    val subtotal = range match {
+      case StillActive(since)        => currentYear - since
+      case ActiveBetween(start, end) => end - start
+    }
+    total + subtotal
+  )
+}
+
+enum SearchCondition {
+  case SearchByGenre(genres: List[MusicGenre])
+  case SearchByOrigin(locations: List[Location])
+  case SearchByActiveYears(start: Int, end: Int)
+  case SearchByTotalYears(years: Int, currentYear: Int)
+}
+
+case class DateRange(start: Int, end: Int)
+
+def wasArtistActive(artist: Artist, range: DateRange): Boolean = {
+  artist.yearsActiveList.exists(artistRange =>
+    artistRange match {
+      case ActiveBetween(start, end) =>
+        start >= range.start && end <= range.end
+      case StillActive(since) =>
+        since >= range.start
+    }
+  )
+}
+
+def searchArtists(
+    artists: List[Artist],
+    requiredConditions: List[SearchCondition]
+): List[Artist] = {
+  artists.filter(artist => {
+    requiredConditions.forall(condition => {
+      condition match {
+        case SearchCondition.SearchByGenre(genres) =>
+          genres.contains(artist.genre)
+        case SearchCondition.SearchByOrigin(locations) =>
+          locations.contains(artist.location)
+        case SearchCondition.SearchByActiveYears(start, end) =>
+          wasArtistActive(artist, DateRange(start, end))
+        case SearchCondition.SearchByTotalYears(years, currentYear) =>
+          activeYears(artist, currentYear) >= years
+      }
+    })
+  })
 }
 
 object ch07_Artist extends App {
   // 7.29 実習: パターンマッチング
   assert(
-    activeSearch(
+    activeYears(
       Artist(
         "Metallica",
         MusicGenre.HeavyMetal,
         Location("U.S."),
-        StillActive(1981)
+        List(StillActive(1981))
       ),
       2022
     ) == 41
   )
 
   assert(
-    activeSearch(
+    activeYears(
       Artist(
         "The Beatles",
         MusicGenre.Pop,
         Location("U.K."),
-        ActiveBetween(1960, 1970)
+        List(ActiveBetween(1960, 1970))
       ),
       2022
     ) == 10
+  )
+
+  // 7.37 コーヒーブレイク: 設計と保守性
+  val metallica = Artist(
+    "Metallica",
+    MusicGenre.HeavyMetal,
+    Location("U.S."),
+    List(StillActive(1981))
+  )
+  val beatles = Artist(
+    "The Beatles",
+    MusicGenre.Pop,
+    Location("U.K."),
+    List(ActiveBetween(1960, 1970))
+  )
+  val beeGees = Artist(
+    "Bee Gees",
+    MusicGenre.Pop,
+    Location("U.K."),
+    List(ActiveBetween(1958, 2003), ActiveBetween(2009, 2012))
+  )
+  val artists = List(
+    metallica,
+    beatles,
+    beeGees
+  )
+
+  assert(
+    searchArtists(
+      artists,
+      List(
+        SearchCondition.SearchByGenre(List(MusicGenre.Pop)),
+        SearchCondition.SearchByOrigin(List(Location("U.K."))),
+        SearchCondition.SearchByActiveYears(1960, 1970)
+      )
+    ) == List(
+      beatles
+    )
+  )
+
+  assert(
+    searchArtists(
+      artists,
+      List(
+        SearchCondition.SearchByActiveYears(2008, 2013)
+      )
+    ) == List(beeGees)
+  )
+
+  assert(
+    searchArtists(
+      artists,
+      List(
+        SearchCondition.SearchByTotalYears(40, 2022)
+      )
+    ) == List(metallica, beeGees)
   )
 }
