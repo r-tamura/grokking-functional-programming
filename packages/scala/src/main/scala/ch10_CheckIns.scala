e9cdd1353d0a1447403dc5d38ddaa511fcf6114b
rutamu30 <rtamura30@gmail.com>
2024-07-07 07:25:34 +0000

    10.22 コーヒーブレイク: 並行的に考える

--- packages/scala/src/main/scala/ch10_CheckIns.scala
+++ packages/scala/src/main/scala/ch10_CheckIns.scala
@@ -5,6 +5,7 @@ import fs2.Stream
 
 import java.util.concurrent.TimeUnit
 import scala.concurrent.duration._
+import ch10_04.topCities
 
 object ch10_CheckIns {
 
@@ -55,10 +56,7 @@ import ch10_CheckIns.model.City
 
 object ch10_04 {
 
-  case class CityStats(
-      city: City,
-      checkIns: Int
-  )
+  import ch10_CheckIns.model._
 
   def topCities(cityCheckIns: Map[City, Int]): List[CityStats] = {
     cityCheckIns.toList
@@ -87,20 +85,81 @@ object ch10_04 {
   }
 }
 
+object ch10_21 {
+
+  import ch10_CheckIns.model._
+
+  def storeCheckIn(storedCheckIns: Ref[IO, Map[City, Int]])(
+      city: City
+  ): IO[Unit] = {
+    storedCheckIns.update(
+      _.updatedWith(city)(optionCount =>
+        optionCount.map(count => count + 1).orElse(Some(1))
+      )
+    )
+  }
+
+  def updateRanking(
+      storedCheckIns: Ref[IO, Map[City, Int]],
+      storedRanking: Ref[IO, List[CityStats]]
+  ): IO[Nothing] = {
+    (for {
+      newRanking <- storedCheckIns.get.map(topCities)
+      _ <- storedRanking.set(newRanking)
+    } yield ()).foreverM
+  }
+
+  def output(
+      storedRanking: Ref[IO, List[CityStats]]
+  )(v: Unit): IO[Unit] = {
+    storedRanking.get.flatMap(IO.println)
+  }
+
+  def processCheckIns(checkIns: Stream[IO, City]): IO[Unit] = {
+    for {
+      storedCheckIns <- Ref.of[IO, Map[City, Int]](Map())
+      storedRanking <- Ref.of[IO, List[CityStats]](List.empty)
+      rankingProgram = updateRanking(storedCheckIns, storedRanking)
+      checkInsProgram = checkIns
+        .evalMap(storeCheckIn(storedCheckIns))
+        .compile
+        .drain
+      outputProgram = IO
+        .sleep(1.second)
+        .flatMap(_ => storedCheckIns.get)
+        .flatMap(IO.println)
+        .foreverM
+      _ <- List(rankingProgram, checkInsProgram, outputProgram).parSequence
+    } yield IO.pure(())
+  }
+}
+
 object ch10_CheckInsApp extends App {
+  val sixCheckIns = Stream(
+    City("Sydney"),
+    City("Sydney"),
+    City("Cape Town"),
+    City("Singapore"),
+    City("Cape Town"),
+    City("Sydney")
+  )
+
   // コーヒーブレイク: 逐次的に考える
   import ch10_04._
-  processCheckIns(
-    Stream(
-      City("Sydney"),
-      City("Sydney"),
-      City("Cape Town"),
-      City("Singapore"),
-      City("Cape Town"),
-      City("Sydney")
-    )
-  ).unsafeRunSync()
+  processCheckIns(sixCheckIns).unsafeRunSync()
 
-  // 実習: 同時IO
+  // 10.21 コーヒーブレイク: 並行的に考える
+  ch10_21
+    .processCheckIns(
+      Stream(
+        City("Sydney"),
+        City("Sydney"),
+        City("Cape Town"),
+        City("Singapore"),
+        City("Cape Town"),
+        City("Sydney")
+      )
+    )
+    .unsafeRunSync()
 
 }
